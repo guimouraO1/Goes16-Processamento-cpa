129af1a940d91423e2a086158e5054fa543891b8
Guilherme <guimoura@unicamp.br>
2023-08-11 13:03:52 -0300

    "Arrumar isso"

---
+++ output/limpar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+find /home/guimoura/download_amazon/output/band* -type f -name "*.gif" -exec rm {} \;
+find /home/guimoura/download_amazon/output/band* -type f -name "*.png" -exec rm {} \;
---
+++ utilities.py
@@ -0,0 +1,276 @@
+#!/opt/miniconda3/envs/goes/bin/python3
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------------------------------------
+# Required modules
+import os  # Miscellaneous operating system interfaces
+import numpy as np  # Import the Numpy package
+import colorsys  # To make convertion of colormaps
+import boto3  # Amazon Web Services (AWS) SDK for Python
+from botocore import UNSIGNED  # boto3 config
+from botocore.config import Config  # boto3 config
+import math  # Mathematical functions
+from datetime import datetime  # Basic Dates and time types
+from osgeo import osr  # Python bindings for GDAL
+from osgeo import gdal  # Python bindings for GDAL
+# -----------------------------------------------------------------------------------------------------------
+
+
+def load_cpt(path):
+    try:
+        f = open(path)
+    except:
+        print("File ", path, "not found")
+        return None
+
+    lines = f.readlines()
+
+    f.close()
+
+    x = np.array([])
+    r = np.array([])
+    g = np.array([])
+    b = np.array([])
+
+    colorModel = 'RGB'
+
+    for l in lines:
+        ls = l.split()
+        if l[0] == '#':
+            if ls[-1] == 'HSV':
+                colorModel = 'HSV'
+                continue
+            else:
+                continue
+        if ls[0] == 'B' or ls[0] == 'F' or ls[0] == 'N':
+            pass
+        else:
+            x = np.append(x, float(ls[0]))
+            r = np.append(r, float(ls[1]))
+            g = np.append(g, float(ls[2]))
+            b = np.append(b, float(ls[3]))
+            xtemp = float(ls[4])
+            rtemp = float(ls[5])
+            gtemp = float(ls[6])
+            btemp = float(ls[7])
+
+        x = np.append(x, xtemp)
+        r = np.append(r, rtemp)
+        g = np.append(g, gtemp)
+        b = np.append(b, btemp)
+
+    if colorModel == 'HSV':
+        for i in range(r.shape[0]):
+            rr, gg, bb = colorsys.hsv_to_rgb(r[i] / 360., g[i], b[i])
+        r[i] = rr;
+        g[i] = gg;
+        b[i] = bb
+
+    if colorModel == 'RGB':
+        r = r / 255.0
+        g = g / 255.0
+        b = b / 255.0
+
+    xNorm = (x - x[0]) / (x[-1] - x[0])
+
+    red = []
+    blue = []
+    green = []
+
+    for i in range(len(x)):
+        red.append([xNorm[i], r[i], r[i]])
+        green.append([xNorm[i], g[i], g[i]])
+        blue.append([xNorm[i], b[i], b[i]])
+
+    colorDict = {'red': red, 'green': green, 'blue': blue}
+
+    return colorDict
+# -----------------------------------------------------------------------------------------------------------
+
+
+def download_cmi(yyyymmddhhmn, band, path_dest, logging):
+    os.makedirs(path_dest, exist_ok=True)
+
+    year = datetime.strptime(yyyymmddhhmn, '%Y%m%d%H%M').strftime('%Y')
+    day_of_year = datetime.strptime(yyyymmddhhmn, '%Y%m%d%H%M').strftime('%j')
+    hour = datetime.strptime(yyyymmddhhmn, '%Y%m%d%H%M').strftime('%H')
+    min = datetime.strptime(yyyymmddhhmn, '%Y%m%d%H%M').strftime('%M')
+
+    # AMAZON repository information
+    # https://noaa-goes16.s3.amazonaws.com/index.html
+    bucket_name = 'noaa-goes16'
+    product_name = 'ABI-L2-CMIPF'
+
+    # Initializes the S3 client
+    s3_client = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+    # -----------------------------------------------------------------------------------------------------------
+    # File structure
+    prefix = f'{product_name}/{year}/{day_of_year}/{hour}/OR_{product_name}-M6C{int(band):02.0f}_G16_s{year}{day_of_year}{hour}{min}'
+
+    # Seach for the file on the server
+    s3_result = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix, Delimiter="/")
+
+    # -----------------------------------------------------------------------------------------------------------
+    # Check if there are files available
+    if 'Contents' not in s3_result:
+        # There are no files
+        ##print(f'No files found for the date: {yyyymmddhhmn}, Band-{band}')
+        logging.info(f'No files found for the date: {yyyymmddhhmn}, Band-{band}')
+        return -1
+    else:
+        # There are files
+        for obj in s3_result['Contents']:
+            key = obj['Key']
+            file_name = key.split('/')[-1].split('.')[0]
+            #Alteração problema servidor
+            new_file_name = file_name.replace('OR','CG')
+
+            # Download the file
+            if os.path.exists(f'{path_dest}/{new_file_name}.nc'):
+                ##print(f'File {path_dest}/{file_name}.nc exists')
+                logging.info(f'File {path_dest}/{file_name}.nc exists')
+            else:
+                ##print(f'Downloading file {path_dest}/{file_name}.nc')
+                logging.info(f'Downloading file {path_dest}/{file_name}.nc')
+                s3_client.download_file(bucket_name, key, f'{path_dest}/{file_name}.nc')
+
+            # Alteração problema servidor
+            old_file = os.path.join(path_dest, f'{file_name}.nc')
+            new_file = os.path.join(path_dest, f'{new_file_name}.nc')
+            os.rename(old_file, new_file)
+
+    return f'{file_name}'
+# -----------------------------------------------------------------------------------------------------------
+
+
+def download_prod(yyyymmddhhmn, product_name, path_dest, logging):
+    os.makedirs(path_dest, exist_ok=True)
+
+    year = datetime.strptime(yyyymmddhhmn, '%Y%m%d%H%M').strftime('%Y')
+    day_of_year = datetime.strptime(yyyymmddhhmn, '%Y%m%d%H%M').strftime('%j')
+    hour = datetime.strptime(yyyymmddhhmn, '%Y%m%d%H%M').strftime('%H')
+    min = datetime.strptime(yyyymmddhhmn, '%Y%m%d%H%M').strftime('%M')
+
+    # AMAZON repository information
+    # https://noaa-goes16.s3.amazonaws.com/index.html
+    bucket_name = 'noaa-goes16'
+
+    # Initializes the S3 client
+    s3_client = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+    # -----------------------------------------------------------------------------------------------------------
+    # File structure
+    prefix = f'{product_name}/{year}/{day_of_year}/{hour}/OR_{product_name}-M6_G16_s{year}{day_of_year}{hour}{min}'
+
+    # Seach for the file on the server
+    s3_result = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix, Delimiter="/")
+
+    # -----------------------------------------------------------------------------------------------------------
+    # Check if there are files available
+    if 'Contents' not in s3_result:
+        # There are no files
+        ## print(f'No files found for the date: {yyyymmddhhmn}, Product-{product_name}')
+        logging.info(f'No files found for the date: {yyyymmddhhmn}, Product-{product_name}')
+        return -1
+    else:
+        # There are files
+        for obj in s3_result['Contents']:
+            key = obj['Key']
+            # Print the file name
+            file_name = key.split('/')[-1].split('.')[0]
+            # Alteração problema servidor
+            new_file_name = file_name.replace('OR', 'CG')
+            # print(file_name)
+            # print(new_file_name)
+
+            # Download the file
+            if os.path.exists(f'{path_dest}/{new_file_name}.nc'):
+                ##print(f'File {path_dest}{file_name}.nc exists')
+                logging.info(f'File {path_dest}{file_name}.nc exists')
+            else:
+                ##print(f'Downloading file {path_dest}{file_name}.nc')
+                logging.info(f'Downloading file {path_dest}{file_name}.nc')
+                s3_client.download_file(bucket_name, key, f'{path_dest}/{file_name}.nc')
+
+            # Alteração problema servidor
+            old_file = os.path.join(path_dest, f'{file_name}.nc')
+            new_file = os.path.join(path_dest, f'{new_file_name}.nc')
+            os.rename(old_file, new_file)
+
+    return f'{file_name}'
+# -----------------------------------------------------------------------------------------------------------
+
+
+def download_glm(yyyymmddhhmnss, path_dest):
+    os.makedirs(path_dest, exist_ok=True)
+
+    year = datetime.strptime(yyyymmddhhmnss, '%Y%m%d%H%M%S').strftime('%Y')
+    day_of_year = datetime.strptime(yyyymmddhhmnss, '%Y%m%d%H%M%S').strftime('%j')
+    hour = datetime.strptime(yyyymmddhhmnss, '%Y%m%d%H%M%S').strftime('%H')
+    min = datetime.strptime(yyyymmddhhmnss, '%Y%m%d%H%M%S').strftime('%M')
+    seg = datetime.strptime(yyyymmddhhmnss, '%Y%m%d%H%M%S').strftime('%S')
+
+    # AMAZON repository information
+    # https://noaa-goes16.s3.amazonaws.com/index.html
+    bucket_name = 'noaa-goes16'
+
+    # Initializes the S3 client
+    s3_client = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+    # -----------------------------------------------------------------------------------------------------------
+    # File structure
+    product_name = "GLM-L2-LCFA"
+    prefix = f'{product_name}/{year}/{day_of_year}/{hour}/OR_{product_name}_G16_s{year}{day_of_year}{hour}{min}{seg}'
+
+    # Seach for the file on the server
+    s3_result = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix, Delimiter="/")
+
+    # -----------------------------------------------------------------------------------------------------------
+    # Check if there are files available
+    if 'Contents' not in s3_result:
+        # There are no files
+        print(f'No files found for the date: {yyyymmddhhmnss}, Product-{product_name}')
+        return -1
+    else:
+        # There are files
+        for obj in s3_result['Contents']:
+            key = obj['Key']
+            # Print the file name
+            file_name = key.split('/')[-1].split('.')[0]
+
+            # Download the file
+            if os.path.exists(f'{path_dest}/{file_name}.nc'):
+                print(f'File {path_dest}/{file_name}.nc exists')
+            else:
+                print(f'Downloading file {path_dest}/{file_name}.nc')
+                s3_client.download_file(bucket_name, key, f'{path_dest}/{file_name}.nc')
+    return f'{file_name}'
+# -----------------------------------------------------------------------------------------------------------
+
+
+# Function to reproject the data
+def reproject(file_name, ncfile, array, extent, undef):
+    # Read the original file projection and configure the output projection
+    source_prj = osr.SpatialReference()
+    source_prj.ImportFromProj4(ncfile.GetProjectionRef())
+
+    target_prj = osr.SpatialReference()
+    target_prj.ImportFromProj4("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+
+    # Reproject the data
+    GeoT = ncfile.GetGeoTransform()
+    driver = gdal.GetDriverByName('MEM')
+    raw = driver.Create('raw', array.shape[0], array.shape[1], 1, gdal.GDT_Float32)
+    raw.SetGeoTransform(GeoT)
+    raw.GetRasterBand(1).WriteArray(array)
+
+    # Define the parameters of the output file  
+    kwargs = {'format': 'netCDF',
+              'srcSRS': source_prj,
+              'dstSRS': target_prj,
+              'outputBounds': (extent[0], extent[3], extent[2], extent[1]),
+              'outputBoundsSRS': target_prj,
+              'outputType': gdal.GDT_Float32,
+              'srcNodata': undef,
+              'dstNodata': 'nan',
+              'resampleAlg': gdal.GRA_NearestNeighbour}
+
+    # Write the reprojected file on disk
+    gdal.Warp(file_name, raw, **kwargs)
