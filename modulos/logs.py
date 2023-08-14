52d1ddb0490c407e174301b426ed68ed43b89fb5
Guilherme <guimoura@unicamp.br>
2023-08-14 10:09:40 -0300

    "finalize start"

---
+++ modulos/logs.py
@@ -0,0 +1,31 @@
+import datetime
+import logging
+import time
+
+
+def conf_log(arq_log):
+        # Configurando log
+        logging.basicConfig(filename=arq_log, level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s", datefmt="%d/%m/%Y %H:%M:%S")
+        # Capturando data/hora inicio
+        inicio = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+        logging.info("")
+        logging.info("")
+        logging.info("=========================================================================================================")
+        logging.info("=                             PROCESSANDO IMAGENS GOES AS " + inicio + "                           =")
+        logging.info("=========================================================================================================")
+        logging.info("")
+
+
+
+def finalize_log_time(s):
+    # Capturando data/hora final
+    fim = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+    # Realiza o log do calculo do tempo de execucao
+    logging.info("")
+    logging.info("Tempo gasto " + str(round(time.time() - s, 4)) + ' segundos')
+    logging.info("")
+    logging.info("=========================================================================================================")
+    logging.info("=                         PROCESSAMENTO ENCERRADO GOES AS " + fim + "                           =")
+    logging.info("=========================================================================================================")
+    logging.info("")
+    logging.info("")
