e14b7682b5a9ff4c78f5ebb4b7b7f15bd64b2a2c
Guilherme <guimoura@unicamp.br>
2023-08-21 10:20:28 -0300

    "Funcionando sem erros 16 bandas"

--- modules/send_products.py
+++ modules/send_products.py
@@ -41,86 +41,6 @@ def send_products(s_br, s_sp, dir_out):
                 # Envia o arquivo "gif" para o site
                 scp_client.put(f'{dir_out}band{b}/band{b}_br.gif', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/band{b}/band{b}_br.gif')
 
-            # Envio TrueColor
-            # Cria uma lista com os itens no diretorio dos produtos truecolor que sao arquivos e se encaixa na expressao regular "^truecolor_.+_.+_br.png$"
-            ultima_br = [name for name in os.listdir(f'{dir_out}truecolor') if os.path.isfile(os.path.join(f'{dir_out}truecolor', name)) and re.match('^truecolor_.+_.+_br.png$', name)]
-            # Ordena de forma alfabetica a lista, ficando assim os arquivos mais antigos no comeco
-            ultima_br.sort()
-            # Realiza a inversao da lista, ficando assim os arquivos mais recentes no comeco
-            ultima_br.reverse()
-            # Envia o arquivo "png" mais recente para o site, renomeando no destino
-            scp_client.put(f'{dir_out}truecolor/{ultima_br[0]}', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/truecolor/truecolor_br.png')
-            # Cria um arquivo menor do arquivo "png" mais recente
-            os.system(f'/usr/bin/ffmpeg -y -v warning -i {dir_out}truecolor/{ultima_br[0]} -vf scale=448:321 {dir_out}truecolor/truecolor.png')
-            # Envia o arquivo menor do "png" mais recente para o site
-            scp_client.put(f'{dir_out}truecolor/truecolor.png', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/truecolor/truecolor.png')
-            # Envia o arquivo "gif" para o site
-            scp_client.put(f'{dir_out}truecolor/truecolor_br.gif', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/truecolor/truecolor_br.gif')
-
-            # Envio RRQPEF
-            # Cria uma lista com os itens no diretorio dos produtos rrqpef que sao arquivos e se encaixa na expressao regular "^rrqpef_.+_.+_br.png$"
-            ultima_br = [name for name in os.listdir(f'{dir_out}rrqpef') if os.path.isfile(os.path.join(f'{dir_out}rrqpef', name)) and re.match('^rrqpef_.+_.+_br.png$', name)]
-            # Ordena de forma alfabetica a lista, ficando assim os arquivos mais antigos no comeco
-            ultima_br.sort()
-            # Realiza a inversao da lista, ficando assim os arquivos mais recentes no comeco
-            ultima_br.reverse()
-            # Envia o arquivo "png" mais recente para o site, renomeando no destino
-            scp_client.put(f'{dir_out}rrqpef/{ultima_br[0]}', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/rrqpef/rrqpef_br.png')
-            # Cria um arquivo menor do arquivo "png" mais recente
-            os.system(f'/usr/bin/ffmpeg -y -v warning -i {dir_out}rrqpef/{ultima_br[0]} -vf scale=448:321 {dir_out}rrqpef/rrqpef.png')
-            # Envia o arquivo menor do "png" mais recente para o site
-            scp_client.put(f'{dir_out}rrqpef/rrqpef.png', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/rrqpef/rrqpef.png')
-            # Envia o arquivo "gif" para o site
-            scp_client.put(f'{dir_out}rrqpef/rrqpef_br.gif', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/rrqpef/rrqpef_br.gif')
-
-            # Envio GLM
-            # Cria uma lista com os itens no diretorio dos produtos glm que sao arquivos e se encaixa na expressao regular "^glm_.+_.+_br.png$"
-            ultima_br = [name for name in os.listdir(f'{dir_out}glm') if os.path.isfile(os.path.join(f'{dir_out}glm', name)) and re.match('^glm_.+_.+_br.png$', name)]
-            # Ordena de forma alfabetica a lista, ficando assim os arquivos mais antigos no comeco
-            ultima_br.sort()
-            # Realiza a inversao da lista, ficando assim os arquivos mais recentes no comeco
-            ultima_br.reverse()
-            # Envia o arquivo "png" mais recente para o site, renomeando no destino
-            scp_client.put(f'{dir_out}glm/{ultima_br[0]}', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/glm/glm_br.png')
-            # Cria um arquivo menor do arquivo "png" mais recente
-            os.system(f'/usr/bin/ffmpeg -y -v warning -i {dir_out}glm/{ultima_br[0]} -vf scale=448:321 {dir_out}glm/glm.png')
-            # Envia o arquivo menor do "png" mais recente para o site
-            scp_client.put(f'{dir_out}glm/glm.png', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/glm/glm.png')
-            # Envia o arquivo "gif" para o site
-            scp_client.put(f'{dir_out}glm/glm_br.gif', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/glm/glm_br.gif')
-
-            # Envio NDVI
-            # Cria uma lista com os itens no diretorio dos produtos ndvi que sao arquivos e se encaixa na expressao regular "^ndvi_.+_.+_br.png$"
-            ultima_br = [name for name in os.listdir(f'{dir_out}ndvi') if os.path.isfile(os.path.join(f'{dir_out}ndvi', name)) and re.match('^ndvi_.+_.+_br.png$', name)]
-            # Ordena de forma alfabetica a lista, ficando assim os arquivos mais antigos no comeco
-            ultima_br.sort()
-            # Realiza a inversao da lista, ficando assim os arquivos mais recentes no comeco
-            ultima_br.reverse()
-            # Envia o arquivo "png" mais recente para o site, renomeando no destino
-            scp_client.put(f'{dir_out}ndvi/{ultima_br[0]}', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/ndvi/ndvi_br.png')
-            # Cria um arquivo menor do arquivo "png" mais recente
-            os.system(f'/usr/bin/ffmpeg -y -v warning -i {dir_out}ndvi/{ultima_br[0]} -vf scale=448:321 {dir_out}ndvi/ndvi.png')
-            # Envia o arquivo menor do "png" mais recente para o site
-            scp_client.put(f'{dir_out}ndvi/ndvi.png', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/ndvi/ndvi.png')
-            # Envia o arquivo "gif" para o site
-            scp_client.put(f'{dir_out}ndvi/ndvi_br.gif', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/ndvi/ndvi_br.gif')
-
-            # Envio FDCF
-            # Cria uma lista com os itens no diretorio dos produtos fdcf que sao arquivos e se encaixa na expressao regular "^fdcf_.+_.+_br.png$"
-            ultima_br = [name for name in os.listdir(f'{dir_out}fdcf') if os.path.isfile(os.path.join(f'{dir_out}fdcf', name)) and re.match('^fdcf_.+_.+_br.png$', name)]
-            # Ordena de forma alfabetica a lista, ficando assim os arquivos mais antigos no comeco
-            ultima_br.sort()
-            # Realiza a inversao da lista, ficando assim os arquivos mais recentes no comeco
-            ultima_br.reverse()
-            # Envia o arquivo "png" mais recente para o site, renomeando no destino
-            scp_client.put(f'{dir_out}fdcf/{ultima_br[0]}', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/fdcf/fdcf_br.png')
-            # Cria um arquivo menor do arquivo "png" mais recente
-            os.system(f'/usr/bin/ffmpeg -y -v warning -i {dir_out}fdcf/{ultima_br[0]} -vf scale=448:321 {dir_out}fdcf/fdcf.png')
-            # Envia o arquivo menor do "png" mais recente para o site
-            scp_client.put(f'{dir_out}fdcf/fdcf.png', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/fdcf/fdcf.png')
-            # Envia o arquivo "gif" para o site
-            scp_client.put(f'{dir_out}fdcf/fdcf_br.gif', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/fdcf/fdcf_br.gif')
-
         # Se a variavel de controle de processamento do estado de sao paulo for True, realiza o processamento
         if s_sp:
             logging.info('')
@@ -140,29 +60,6 @@ def send_products(s_br, s_sp, dir_out):
                 # Envia o arquivo "gif" para o site
                 scp_client.put(f'{dir_out}band{b}/band{b}_sp.gif', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/band{b}/band{b}_sp.gif')
 
-            # Envio TrueColor
-            # Cria uma lista com os itens no diretorio dos produtos truecolor que sao arquivos e se encaixa na expressao regular "^truecolor_.+_.+_br.png$"
-            ultima_sp = [name for name in os.listdir(f'{dir_out}truecolor') if os.path.isfile(os.path.join(f'{dir_out}truecolor', name)) and re.match('^truecolor_.+_.+_sp.png$', name)]
-            # Ordena de forma alfabetica a lista, ficando assim os arquivos mais antigos no comeco
-            ultima_sp.sort()
-            # Realiza a inversao da lista, ficando assim os arquivos mais recentes no comeco
-            ultima_sp.reverse()
-            # Envia o arquivo "png" mais recente para o site, renomeando no destino
-            scp_client.put(f'{dir_out}truecolor/{ultima_sp[0]}', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/truecolor/truecolor_sp.png')
-            # Envia o arquivo "gif" para o site
-            scp_client.put(f'{dir_out}truecolor/truecolor_sp.gif', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/truecolor/truecolor_sp.gif')
-
-            # Envio RRQPEF
-            # Cria uma lista com os itens no diretorio dos produtos rrqpef que sao arquivos e se encaixa na expressao regular "^rrqpef_.+_.+_sp.png$"
-            ultima_sp = [name for name in os.listdir(f'{dir_out}rrqpef') if os.path.isfile(os.path.join(f'{dir_out}rrqpef', name)) and re.match('^rrqpef_.+_.+_sp.png$', name)]
-            # Ordena de forma alfabetica a lista, ficando assim os arquivos mais antigos no comeco
-            ultima_sp.sort()
-            # Realiza a inversao da lista, ficando assim os arquivos mais recentes no comeco
-            ultima_sp.reverse()
-            # Envia o arquivo "png" mais recente para o site, renomeando no destino
-            scp_client.put(f'{dir_out}rrqpef/{ultima_sp[0]}', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/rrqpef/rrqpef_sp.png')
-            # Envia o arquivo "gif" para o site
-            scp_client.put(f'{dir_out}rrqpef/rrqpef_sp.gif', f'/var/www/html/cepagri/atualizacoes-regulares/goes16/rrqpef/rrqpef_sp.gif')
 
     except TimeoutError as e_timeout:
         logging.info('')
