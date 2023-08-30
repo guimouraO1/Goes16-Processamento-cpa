1fcf168daa478a2dcc7fe53bfa1dc72235913125
Guilherme <guimoura@unicamp.br>
2023-08-30 08:05:12 -0300

    "confirma"

--- teste.py
+++ teste.py
@@ -6,17 +6,16 @@ ch13 = 'CG_ABI-L2-CMIPF-M6C13_G16_s20232411440206_e20232411449526_c2023241145227
 dir_in = '/home/guimoura/download_amazon/goes/'
 
 
-
 # Cria uma lista com os itens presentes no diretório da banda que são arquivos e terminam com ".nc"
 images = [f for f in os.listdir(f'{dir_in}glm') if os.path.isfile(os.path.join(f'{dir_in}glm', f)) and re.match('^OR_GLM-L2-LCFA_G16_s.+_e.+_c.+.nc$', f)]
 images.sort()
             
 glm_list = []
 
-ch13 = (datetime.datetime.strptime(ch13[ch13.find("M6C13_G16_s") + 11:ch13.find("_e") - 1], '%Y%j%H%M%S'))
-print(ch13)
-date_ini = datetime.datetime(ch13.year, ch13.month, ch13.day, ch13.hour, ch13.minute)
-date_end = datetime.datetime(ch13.year, ch13.month, ch13.day, ch13.hour, ch13.minute) + datetime.timedelta(minutes=9, seconds=59)
+ch13_data = (datetime.datetime.strptime(ch13[ch13.find("M6C13_G16_s") + 11:ch13.find("_e") - 1], '%Y%j%H%M%S'))
+
+date_ini = datetime.datetime(ch13_data.year, ch13_data.month, ch13_data.day, ch13_data.hour, ch13_data.minute)
+date_end = datetime.datetime(ch13_data.year, ch13_data.month, ch13_data.day, ch13_data.hour, ch13_data.minute) + datetime.timedelta(minutes=9, seconds=59)
 
 for x in images:
     xtime = (datetime.datetime.strptime(x[x.find("GLM-L2-LCFA_G16_s") + 17:x.find("_e") - 1], '%Y%j%H%M%S'))
@@ -24,3 +23,9 @@ for x in images:
         glm_list.append(x)
     else:
         continue
+
+glm_list.insert(0, ch13)
+print(glm_list)
+
+dic_list = {ch13:glm_list}
+print(dic_list)
