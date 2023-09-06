7dfad7308650a313994f4cee818a470ef2ffbc59
Guilherme <guimoura@unicamp.br>
2023-09-06 11:17:36 -0300

    "confirma"

--- download_amazon.py
+++ download_amazon.py
@@ -3,33 +3,40 @@
 
 from osgeo import gdal  # Utilitario para a biblioteca GDAL
 from modules.utilities import download_prod  # Funcao para download dos produtos do goes disponiveis na amazon
-from modules.utilities import download_cmi
+from modules.utilities import download_cmi_joao
 import datetime
 from datetime import timedelta
 import logging
 import os
 import time
+from modules.dirs import get_dirs
+
+dirs = get_dirs()
+
+# Acessando os diretórios usando as chaves do dicionário
+dir_in = dirs['dir_in']
+dir_log = dirs['arq_log']
 
 gdal.PushErrorHandler('CPLQuietErrorHandler')   # Ignore GDAL warnings 
 
-processing_start_time = time.time()
+
+#Inicia contador para o tempo gasto de processamento
+start = time.time()
 
 #Diretórios
-dir_in = "/Scripts/goes16/processamento/goes/"
-arq_log = "/Scripts/goes16/processamento/logs/" + str(datetime.date.today()) + ".log"
+arq_log = '/home/guimoura/download_amazon/logs/' + str(datetime.date.today()) + ".log"
+
 # Configurando log
-logging.basicConfig(filename=arq_log, level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s", datefmt="ls%m/%Y %H:%M:%S")
+logging.basicConfig(filename=arq_log, level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s", datefmt="%d/%m/%Y %H:%M:%S")
 # Capturando data/hora inicio
 inicio = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
 logging.info("")
 logging.info("")
-logging.info("=========================================================================================================")
-logging.info("=                             DOWNLOAD IMAGENS GOES AS " + inicio + "                           =")
-logging.info("=========================================================================================================")
+logging.info("==================================================================================================================")
+logging.info("=                                  DOWNLOAD IMAGENS GOES AS " + inicio + "                                  =")
+logging.info("==================================================================================================================")
 logging.info("")
 
-#Inicia contador para o tempo gasto de processamento
-start = time.time()
 
 #Captura hora atual em UTC para download no site da Amazon
 data_hora_atual = datetime.datetime.utcnow()
@@ -40,293 +47,51 @@ data_10_min = datetime.datetime.strftime(data_hora_atual-timedelta(minutes=10),'
 #Correção para poder fazer download em qualquer horário
 data_hora_download_file = data_10_min[0:11]+ '0'
 
-# #Download band01
-download_cmi(data_hora_download_file,1,dir_in +'band01')
-
-# #Download band02
-download_cmi(data_hora_download_file,2,dir_in +'band02')
-
-# #Download band03
-download_cmi(data_hora_download_file,3,dir_in +'band03')
-
-# #Download band04
-download_cmi(data_hora_download_file,4,dir_in +'band04')
-
-# #Download band05
-download_cmi(data_hora_download_file,5,dir_in +'band05')
-
-# #Download band06
-download_cmi(data_hora_download_file,6,dir_in +'band06')
-
-# #Download band07
-download_cmi(data_hora_download_file,7,dir_in +'band07')
-
-# #Download band08
-download_cmi(data_hora_download_file,8,dir_in +'band08')
-
-# #Download band09
-download_cmi(data_hora_download_file,9,dir_in +'band09')
-
-# #Download band10
-download_cmi(data_hora_download_file,10,dir_in +'band10')
-
-# #Download band11
-download_cmi(data_hora_download_file,11,dir_in +'band11')
-
-# #Download band12
-download_cmi(data_hora_download_file,12,dir_in +'band12')
-
-# #Download band13
-download_cmi(data_hora_download_file,13,dir_in +'band13')
-
-# #Download band14
-download_cmi(data_hora_download_file,14,dir_in +'band14')
-
-# #Download band15
-download_cmi(data_hora_download_file,15,dir_in +'band15')
-
-# #Download band 16
-download_cmi(data_hora_download_file,16,dir_in +'band16')
-
-#Download band01
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band1 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,1,dir_in +'band01')
-            break
-        except:
-            print("Sem imagens para Band1 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band1 no horário {startb1}")
+# Contador para download das 16 bandas
+for x in range(1,17):
+    # Transforma o inteiro contador em string e com 2 digitos
+    b = str(x).zfill(2)
+    # Download band
+    logging.info("")
+    logging.info(f'Tentando download Band{b}...')
+    try:
+        download_cmi_joao(data_hora_download_file, x, dir_in + f'band{b}', logging)
+    except Exception as e:
+        print(f'{e}')
+        continue
 
 
-
-#Download band02
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band2 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,2,dir_in +'band02')
-            break
-        except:
-            print("Sem imagens para Band2 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band1 no horário {startb1}")
-
-
-
-#Download band03
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band3 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,3,dir_in +'band03')
-            break
-        except:
-            print("Sem imagens para Band3 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band3 no horário {startb1}")
+# Função para renomear arquivos
+def renomear_arquivos(diretorio):
+    for nome_arquivo in os.listdir(diretorio):
+        caminho_arquivo = os.path.join(diretorio, nome_arquivo)
+        
+        # Verifique se é um diretório
+        if os.path.isdir(caminho_arquivo):
+            # Se for um diretório, chame recursivamente a função
+            renomear_arquivos(caminho_arquivo)
+        else:
+            # Verifique se o nome do arquivo contém "OR_"
+            if "OR_" in nome_arquivo:
+                # Construa o novo nome de arquivo substituindo "OR_" por "CG_"
+                novo_nome = nome_arquivo.replace("OR_", "CG_")
+                
+                # Renomeie o arquivo
+                os.rename(caminho_arquivo, os.path.join(diretorio, novo_nome))
 
 
+# Percorra as subpastas "band01" até "band16"
+for band_folder in range(1, 17):
+    subdiretorio = os.path.join(dir_in, f"band{band_folder:02d}")
+    
+    if os.path.exists(subdiretorio):
+        # Chame a função para renomear os arquivos na subpasta
+        renomear_arquivos(subdiretorio)
 
-#Download band04
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band4 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,4,dir_in +'band04')
-            break
-        except:
-            print("Sem imagens para Band4 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band4 no horário {startb1}")
+print("Arquivos renomeados com sucesso!")
 
-
-
-#Download band05
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band5 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,5,dir_in +'band05')
-            break
-        except:
-            print("Sem imagens para Band5 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band5 no horário {startb1}")
-
-
-
-#Download band06
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band6 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,6,dir_in +'band06')
-            break
-        except:
-            print("Sem imagens para Band6 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band6 no horário {startb1}")
-
-
-
-#Download band07
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band7 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,7,dir_in +'band07')
-            break
-        except:
-            print("Sem imagens para Band7 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band7 no horário {startb1}")
-
-
-
-#Download band08
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band8 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,8,dir_in +'band08')
-            break
-        except:
-            print("Sem imagens para Band8 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band8 no horário {startb1}")
-
-
-
-#Download band09
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band9 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,9,dir_in +'band09')
-            break
-        except:
-            print("Sem imagens para Band9 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band9 no horário {startb1}")
-
-
-
-#Download band10
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band10 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,10,dir_in +'band10')
-            break
-        except:
-            print("Sem imagens para Band10 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band10 no horário {startb1}")
-
-
-
-#Download band11
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band11 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,11,dir_in +'band11')
-            break
-        except:
-            print("Sem imagens para Band11 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band11 no horário {startb1}")
-
-
-
-#Download band12
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band12 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,12,dir_in +'band12')
-            break
-        except:
-            print("Sem imagens para Band12 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band12 no horário {startb1}")
-
-
-
-#Download band13
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band13 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,13,dir_in +'band13')
-            break
-        except:
-            print("Sem imagens para Band13 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band13 no horário {startb1}")
-
-
-
-#Download band14
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band14 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,14,dir_in +'band14')
-            break
-        except:
-            print("Sem imagens para Band14 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band14 no horário {startb1}")
-
-
-
-#Download band15
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band15 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,15,dir_in +'band15')
-            break
-        except:
-            print("Sem imagens para Band15 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band15 no horário {startb1}")
-
-
-
-#Download band16
-logging.info("")
-startb1 = datetime.datetime.now()
-logging.info(f'Download Band16 as {startb1.strftime("%d/%m/%Y-%H:%M:%S")}')
-for i in range(0, 3):
-        try:
-            download_cmi(data_hora_download_file,16,dir_in +'band16')
-            break
-        except:
-            print("Sem imagens para Band16 no horário")
-            print("Tentativa de Download {} de 3" .format(i + 1))
-            logging.info(f"Sem imagens para Band16 no horário {startb1}")
-
-
+        
+        
 def finalize(s):
     # Capturando data/hora final
     fim = datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
@@ -334,14 +99,10 @@ def finalize(s):
     logging.info("")
     logging.info("Tempo gasto " + str(round(time.time() - s, 4)) + ' segundos')
     logging.info("")
-    logging.info("=========================================================================================================")
-    logging.info("=                         PROCESSAMENTO ENCERRADO GOES AS " + fim + "                           =")
-    logging.info("=========================================================================================================")
+    logging.info("==================================================================================================================")
+    logging.info("=                             PROCESSAMENTO ENCERRADO GOES AS " + fim + "                                =")
+    logging.info("==================================================================================================================")
     logging.info("")
     logging.info("")
 
 finalize(start)
-
-print(round(time.time() - processing_start_time, 4) / 60)
-
-
--- modules/utilities.py
+++ modules/utilities.py
@@ -250,3 +250,59 @@ def reproject(file_name, ncfile, array, extent, undef):
 
     # Write the reprojected file on disk
     gdal.Warp(file_name, raw, **kwargs)
+
+
+
+def download_cmi_joao(yyyymmddhhmn, band, path_dest, logging):
+    os.makedirs(path_dest, exist_ok=True)
+
+    year = datetime.strptime(yyyymmddhhmn, '%Y%m%d%H%M').strftime('%Y')
+    day_of_year = datetime.strptime(yyyymmddhhmn, '%Y%m%d%H%M').strftime('%j')
+    hour = datetime.strptime(yyyymmddhhmn, '%Y%m%d%H%M').strftime('%H')
+    min = datetime.strptime(yyyymmddhhmn, '%Y%m%d%H%M').strftime('%M')
+
+    # AMAZON repository information
+    # https://noaa-goes16.s3.amazonaws.com/index.html
+    bucket_name = 'noaa-goes16'
+    product_name = 'ABI-L2-CMIPF'
+
+    # Initializes the S3 client
+    s3_client = boto3.client('s3', config=Config(signature_version=UNSIGNED))
+    # -----------------------------------------------------------------------------------------------------------
+    # File structure
+    prefix = f'{product_name}/{year}/{day_of_year}/{hour}/OR_{product_name}-M6C{int(band):02.0f}_G16_s{year}{day_of_year}{hour}{min}'
+
+    # Seach for the file on the server
+    s3_result = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix, Delimiter="/")
+
+    # -----------------------------------------------------------------------------------------------------------
+    # Check if there are files available
+    if 'Contents' not in s3_result:
+        # There are no files
+        ##print(f'No files found for the date: {yyyymmddhhmn}, Band-{band}')
+        logging.info(f'No files found for the date: {yyyymmddhhmn}, Band-{band}')
+        return -1
+    else:
+        # There are files
+        for obj in s3_result['Contents']:
+            key = obj['Key']
+            file_name = key.split('/')[-1].split('.')[0]
+            #Alteração problema servidor
+            new_file_name = file_name.replace('OR','CG')
+
+            # Download the file
+            if os.path.exists(f'{path_dest}/{new_file_name}.nc'):
+                ##print(f'File {path_dest}/{file_name}.nc exists')
+                logging.info(f'File {path_dest}/{file_name}.nc exists')
+            else:
+                ##print(f'Downloading file {path_dest}/{file_name}.nc')
+                logging.info(f'Downloading file {path_dest}/{file_name}.nc')
+                s3_client.download_file(bucket_name, key, f'{path_dest}/{file_name}.nc')
+
+            # Alteração problema servidor
+            old_file = os.path.join(path_dest, f'{file_name}.nc')
+            new_file = os.path.join(path_dest, f'{new_file_name}.nc')
+            os.rename(old_file, new_file)
+
+    return f'{file_name}'
+# -----------------------------------------------------------------------------------------------------------
